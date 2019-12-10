3416f6588f87d652d535f724703dd17d9be1312a
Cameron Marion <CSMarion@student.fullsail.edu>
2019-12-10 16:28:07 -0500

    Created GetAllAccountsByUserID action initial setup.

--- Banking.API/Controllers/AccountsController.cs
+++ Banking.API/Controllers/AccountsController.cs
@@ -24,11 +24,33 @@ namespace Banking.API.Controllers
         }
 
         // GET: api/Accounts
-        [HttpGet]
+        [HttpGet("{id}")]
         [Produces(typeof(object))]
-        public IEnumerable<string> Get()
+        [ProducesResponseType(403)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(500)]
+        public async Task<ActionResult<IEnumerable<object>>> GetAllAccountsByUserID(int id)
         {
-            return new string[] { "value1", "value2" };
+            try
+            {
+                IEnumerable<object> result = null;
+
+                _logger.LogInformation(string.Format("Start GetAllAccountsByUserID: {0}", id.ToString()));
+
+                if (result.Count() < 1)
+                {
+                    _logger.LogWarning(string.Format("No Accounts found for UserID: {0}", id.ToString()));
+                    return NotFound(id);
+                }
+                _logger.LogInformation(string.Format("GetAllAccountsByUserID: {0} Succeded.", id.ToString()));
+
+                return result;
+            }
+            catch (Exception WTF)
+            {
+                _logger?.LogError(WTF, "Unexpected Error in GetAllAccountsByUserID!");
+                return StatusCode(500, WTF);
+            }
         }
 
         // GET: api/Accounts/5
