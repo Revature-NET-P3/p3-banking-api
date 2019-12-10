f64c27f322320562164e4833ad13f1c8c18c21a9
Aaron Adler <aaron.adler@revature.net>
2019-12-10 16:37:36 -0500

    Models Created

---
+++ Banking.API/Models/AccountType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Banking.API.Models
+{
+    public class AccountType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal InterestRate { get; set; }
+    }
+}
---
+++ Banking.API/Repositories/Interfaces/IUserRepo.cs
@@ -0,0 +1,21 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+
+//namespace Banking.API.Repositories.Interfaces
+//{
+//    public class IUserRepo
+//    {
+//        public Task<bool> CreateUser(User user);
+
+//        public Task<>
+
+
+//            CreateUser
+//ViewUserDetailsByID
+//UpdateUserDetails
+//VerifyUserLogIn
+
+//    }
+//}
