f45617f9768463c818925a11c7ef07ab82d60733
Aaron Adler <aaron.adler@revature.net>
2019-12-11 14:10:34 -0500

    Incomplete, Changing UserRepo

---
+++ Banking.API/Repositories/AccountTypeRepo.cs
@@ -0,0 +1,37 @@
+﻿using Banking.API.Models;
+using Banking.API.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Banking.API.Repositories
+{
+    public class AccountTypeRepo : IAccountTypeRepo
+    {
+        private AppDbContext _context;
+
+        public AccountTypeRepo(AppDbContext ctx)
+        {
+            _context = ctx;
+        }
+
+        public Task<IEnumerable<AccountType>> GetAccountTypes()
+        {
+            var AccountTypes = await _context.AccountType
+        }
+        public Task<AccountType> GetAccountTypeById()
+        {
+
+        }
+        public Task<AccountType> GetAccountTypeByName()
+        {
+
+        }
+        public Task<bool> AddAccountType()
+        {
+
+        }
+
+    }
+}
---
+++ Banking.API/Repositories/Interfaces/IAccountTypeRepo.cs
@@ -0,0 +1,16 @@
+﻿using Banking.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Banking.API.Repositories.Interfaces
+{
+    interface IAccountTypeRepo
+    {
+        public Task<IEnumerable<AccountType>> GetAccountTypes();
+        public Task<AccountType> GetAccountTypeById();
+        public Task<AccountType> GetAccountTypeByName();
+        public Task<bool> AddAccountType();
+    }
+}
