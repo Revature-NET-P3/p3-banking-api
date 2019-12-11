aee614fd2a6cefaf1636b5df2292e047488263a0
Cameron Marion <cameron.marion@revature.net>
2019-12-11 16:02:21 -0500

    Create new test AccountTypeRepo for unit testing.

---
+++ Banking.Tests/DataObjects/AccountTypeRepo.cs
@@ -0,0 +1,39 @@
+﻿using Banking.API.Models;
+using Banking.API.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking.Tests.DataObjects
+{
+    class AccountTypeRepo : IAccountTypeRepo
+    {
+        List<AccountType> AccountTypes = null;
+
+        public AccountTypeRepo(bool doFill = true)
+        {
+            
+        }
+
+        public Task<bool> AddAccountType(AccountType accType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AccountType> GetAccountTypeById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AccountType> GetAccountTypeByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<AccountType>> GetAccountTypes()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
