e6545b5f4b3dda120bce6d384d937c1b2e0f92d0
Dinesh Ghimire <dineshg822@gmail.com>
2019-12-11 15:42:10 -0500

    UserAPI Controller

---
+++ Banking.API/Controllers/NewFolder/UserAPIController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Banking.API.Models;
+using Banking.API.Repositories;
+using Banking.API.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Banking.API.Controllers.NewFolder
+{
+    // [EnableCors("DefaultPolicy")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserAPIController : Controller
+    {
+              
+            private readonly UserRepo _context;
+
+            public UserAPIController(UserRepo context)
+            {
+                _context = context;
+            }
+
+            //// GET: api/User
+            //[HttpGet]
+            //public async Task<ActionResult<IEnumerable<User>>> GetUser()
+            //{
+            //    return await _context.SelectAll();
+            //}
+
+            // GET: api/User/5
+            [HttpGet("{id}")]
+            public async Task<ActionResult<User>> GetUser(int id)
+            {
+                var user = await _context.ViewById(id);
+
+                if (user == null)
+                {
+                    return NotFound();
+                }
+
+                return user;
+            }
+
+            //[HttpGet("ByUser/{Username}")]
+            //public async Task<ActionResult<User>> GetCustomerByUser(string Username)
+            //{
+            //    var customer = await _context.SelectByUser(Username);
+
+            //    if (customer == null)
+            //    {
+            //        return NotFound();
+            //    }
+
+            //    return customer;
+            //}
+
+            // PUT: api/User/5
+            // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+            // more details see https://aka.ms/RazorPagesCRUD.
+            [HttpPut("{id}")]
+            public async Task<IActionResult> PutCustomer(int id, User user)
+            {
+                if (id != user.Id)
+                {
+                    return BadRequest();
+                }
+
+
+                try
+                {
+                    await _context.UpdateUser(user);
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UserExists(id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+
+                return NoContent();
+            }
+
+            // POST: api/User
+            // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+            // more details see https://aka.ms/RazorPagesCRUD.
+            [HttpPost]
+            public async Task<ActionResult<User>> PostCustomer(User user)
+            {
+           
+                await _context.CreateUser(user);
+
+
+                return CreatedAtAction("GetUser", new { id = user.Id }, user);
+            }
+
+            //// DELETE: api/User/5
+            //[HttpDelete("{id}")]
+            //public async Task<ActionResult<User>> DeleteCustomer(int id)
+            //{
+            //    var user = await _context.SelectById(id);
+            //    if (user == null)
+            //    {
+            //        return NotFound();
+            //    }
+
+            //    await _context.Remove(user);
+
+            //    return user;
+            //}
+
+            //GET: api/
+
+            private bool UserExists(int id)
+            {
+                return _context.UserExists(id);
+            }
+        
+    }
+
+
+    
+}
---
+++ Banking.API/Controllers/NewFolder/UserController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Banking.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Banking.API.Controllers
+{
+    public class UserController : Controller
+    {
+
+        private readonly AppDbContext _context;
+
+        public UserController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: User
+        public async Task<IActionResult> Index()
+        {
+            var AppDbContext = _context.Users;
+            return View(await AppDbContext.ToListAsync());
+        }
+
+        // GET: User/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Users
+                      .FirstOrDefaultAsync(m => m.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        // GET: User/Create
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+
+        // POST: User/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,UserName,Password, Email")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(user);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ;
+            return View(user);
+        }
+
+        // GET: User/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+           
+            return View(user);
+        }
+
+        // POST: User/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,UserName,Password, Email")] User user)
+        {
+            if (id != user.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(user);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UserExists(user.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+          
+            return View(user);
+        }
+
+        //// GET: Customers/Delete/5
+        //public async Task<IActionResult> Delete(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    var user = await _context.Users              
+        //        .FirstOrDefaultAsync(m => m.Id == id);
+        //    if (user == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    return View(user);
+        //}
+
+        //// POST: Customers/Delete/5
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> DeleteConfirmed(int id)
+        //{
+        //    var user = await _context.Users.FindAsync(id);
+        //    _context.Users.Remove(user);
+        //    await _context.SaveChangesAsync();
+        //    return RedirectToAction(nameof(Index));
+        //}
+
+        private bool UserExists(int id)
+        {
+            return _context.Users.Any(e => e.Id == id);
+        }
+
+
+
+    }
+}
