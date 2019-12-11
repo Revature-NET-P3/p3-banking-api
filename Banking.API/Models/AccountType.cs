e5afbf7fcafc22635311e7e07ff064edad1206d1
Aaron Adler <aaron.adler@revature.net>
2019-12-11 11:05:17 -0500

    Added data annotations to models, made Username uniqe index

--- Banking.API/Models/AccountType.cs
+++ Banking.API/Models/AccountType.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,11 @@ namespace Banking.API.Models
 {
     public class AccountType
     {
+        [Required, Key]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public decimal InterestRate { get; set; }
     }
 }
