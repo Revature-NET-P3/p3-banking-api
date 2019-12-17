5c60e73e5db478e8f534e32843c4e56846d09229
Cameron Marion <cameron.marion@revature.net>
2019-12-17 09:20:41 -0500

    Minor using cleanup in AccountRepoTest.

--- Banking.Tests/DataObjects/AccountRepoTest.cs
+++ Banking.Tests/DataObjects/AccountRepoTest.cs
@@ -1,11 +1,10 @@
-﻿using Banking.API.Models;
-using Banking.API.Repositories.Interfaces;
-using Microsoft.EntityFrameworkCore;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+
+using Banking.API.Models;
+using Banking.API.Repositories.Interfaces;
 
 namespace Banking.Tests.DataObjects
 {
