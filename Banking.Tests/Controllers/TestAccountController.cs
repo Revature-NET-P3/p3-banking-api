cdca16c00a6528bd0f0169fef4493f05c1b23120
Cameron Marion <cameron.marion@revature.net>
2019-12-17 10:20:39 -0500

    Removed duplicate using from TestAccountController.

--- Banking.Tests/Controllers/TestAccountController.cs
+++ Banking.Tests/Controllers/TestAccountController.cs
@@ -3,7 +3,6 @@ using Microsoft.Extensions.Logging;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 using Moq;
-using System.Collections.Generic;
 
 using Banking.API.Controllers;
 using Banking.API.Models;
